800ade60e3f8c5f8ebaa62be1bb73f19a07493cf
David Buezas <david.buezas@gmail.com>
2022-10-02 17:03:29 +0200

    Implements accurate delayMicroseconds

--- arduino/beken-72xx/cores/arduino/wiring.c
+++ arduino/beken-72xx/cores/arduino/wiring.c
@@ -11,13 +11,46 @@ void delayMilliseconds(unsigned long ms) {
 	rtos_delay_milliseconds(ms);
 }
 
+
+uint32_t get_ticks_count(){
+	uint32_t timeout = 0;
+	REG_WRITE(TIMER0_2_READ_CTL, (BKTIMER0 << 2) | 1);
+	while (REG_READ(TIMER0_2_READ_CTL) & 1) {
+		timeout++;
+		if (timeout > (120 * 1000))
+			return 0;
+	}
+	return REG_READ(TIMER0_2_READ_VALUE);
+}
+#define TICKS_PER_US (CFG_XTAL_FREQUENCE / 1000 / 1000)
+#define TIMER0_US_OVERFLOW (portTICK_PERIOD_MS * 1000)
+#define TICKS_PER_OVERFLOW (TICKS_PER_US * TIMER0_US_OVERFLOW)
+
 void delayMicroseconds(unsigned int us) {
-	// TODO implement this properly
-	us /= 10;
+#if LT_MICROS_HIGH_RES
+	if (us == 0) return;
+	us--;
+	uint32_t start_tick = get_ticks_count();
+	/* start_tick_2 accounts for the case where the timer counter overflows */
+	uint32_t start_tick_2 = start_tick - TICKS_PER_OVERFLOW; 
+	uint32_t delay_ticks = TICKS_PER_US * us;
+	while (delay_ticks > TICKS_PER_OVERFLOW) {
+		// The delay is longer than what the timer can count.
+		// Let it overflow until only a fraction of TICKS_PER_OVERFLOW remain.
+		while (get_ticks_count() > start_tick) {}
+		while (get_ticks_count() < start_tick) {}
+		delay_ticks -= TICKS_PER_OVERFLOW;
+	}
+	while (
+		(get_ticks_count() - start_tick < delay_ticks) || // normal case
+		(get_ticks_count() - start_tick_2 < delay_ticks) // counter overflow case
+	) {}
+#else 
 	volatile uint32_t i, j;
 	for (i = 0; i < us; i++) {
-		for (j = 0; j < 100; j++) {}
+		for (j = 0; j < 6; j++) {}
 	}
+#endif
 }
 
 unsigned long millis() {
@@ -33,13 +66,7 @@ unsigned long micros() {
 #endif
 
 #if LT_MICROS_HIGH_RES
-	REG_WRITE(TIMER0_2_READ_CTL, (BKTIMER0 << 2) | 1);
-	while (REG_READ(TIMER0_2_READ_CTL) & 1) {
-		timeout++;
-		if (timeout > (120 * 1000))
-			return 0;
-	}
-	return millis() * 1000 + REG_READ(TIMER0_2_READ_VALUE) / (CFG_XTAL_FREQUENCE / 1000 / 1000);
+	return millis() * 1000 + get_ticks_count() / (CFG_XTAL_FREQUENCE / 1000 / 1000);
 #else
 #if 0
 	REG_WRITE(TIMER3_5_READ_CTL, (BKTIMER3 << 2) | 1);
