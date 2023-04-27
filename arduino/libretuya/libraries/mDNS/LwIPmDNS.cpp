988d358f9ec22f4ef11155d969a47e86f5031ef5
Stroe Andrei Catalin <catalin.stroe@conceptapps.ro>
2023-04-27 17:51:59 +0300

    [libs] Fix mDNS not responding when device disconnects / reconnects to wifi

--- arduino/libretuya/libraries/mDNS/LwIPmDNS.cpp
+++ arduino/libretuya/libraries/mDNS/LwIPmDNS.cpp
@@ -13,9 +13,16 @@ extern "C" {
 #include <lwip/netif.h>
 }
 
+static std::vector<char *> services_name;
 static std::vector<char *> services;
 static std::vector<uint8_t> protos;
+static std::vector<uint16_t> ports;
 static std::vector<std::vector<char *>> records;
+
+const char *hostName;
+#ifdef LWIP_NETIF_EXT_STATUS_CALLBACK
+NETIF_DECLARE_EXT_CALLBACK(netif_callback)
+#endif
 
 mDNS::mDNS() {}
 
@@ -37,34 +44,89 @@ static void mdnsStatusCallback(struct netif *netif, uint8_t result) {
 	LT_DM(MDNS, "Status: netif %u, status %u", netif->num, result);
 }
 
+#ifdef LWIP_NETIF_EXT_STATUS_CALLBACK
+static void readdServices(struct netif *netif) {
+	for (uint8_t i = 0; i < services.size(); i++) {
+		LT_DM(
+			MDNS,
+			"Readd service: netif %u / %s / %s / %u / %u",
+			netif->num,
+			services_name[i],
+			services[i],
+			protos[i],
+			ports[i]
+		);
+		mdns_resp_add_service(
+			netif,
+			services_name[i],
+			services[i],
+			(mdns_sd_proto)protos[i],
+			ports[i],
+			255,
+			mdnsTxtCallback,
+			reinterpret_cast<void *>(i) // index of newly added service
+		);
+	}
+}
+#endif
+
+static bool start() {
+	uint8_t enabled = 0;
+	struct netif *netif;
+	for (netif = netif_list; netif != NULL; netif = netif->next) {
+		LT_DM(MDNS, "Stopping mDNS & IGMP on netif %u", netif->num);
+		igmp_stop(netif);
+		mdns_resp_remove_netif(netif);
+		LT_DM(MDNS, "Stopped mDNS & IGMP on netif %u", netif->num);
+		if (netif_is_up(netif)) {
+			LT_DM(MDNS, "Starting mDNS on netif %u", netif->num);
+			if ((netif->flags & NETIF_FLAG_IGMP) == 0) {
+				LT_DM(MDNS, "Starting IGMP on netif %u", netif->num);
+				netif->flags |= NETIF_FLAG_IGMP;
+				igmp_start(netif);
+			}
+			err_t ret = mdns_resp_add_netif(netif, hostName, 255);
+			if (ret == ERR_OK) {
+				LT_DM(MDNS, "Started mDNS & IGMP on netif %u, announcing it to network", netif->num);
+#ifdef LWIP_NETIF_EXT_STATUS_CALLBACK
+				if (services.size() > 0) {
+					LT_DM(MDNS, "Readding Services");
+					readdServices(netif);
+				}
+#endif
+				mdns_resp_announce(netif);
+				enabled++;
+			} else
+				LT_DM(MDNS, "Cannot start mDNS on netif %u; ret=%d, errno=%d", netif->num, ret, errno);
+		}
+
+		return enabled > 0;
+	}
+}
+
+#ifdef LWIP_NETIF_EXT_STATUS_CALLBACK
+static void
+mdns_netif_ext_status_callback(struct netif *netif, netif_nsc_reason_t reason, const netif_ext_callback_args_t *args) {
+	if (reason & (LWIP_NSC_IPV4_ADDRESS_CHANGED | LWIP_NSC_IPV4_GATEWAY_CHANGED | LWIP_NSC_IPV4_NETMASK_CHANGED |
+				  LWIP_NSC_IPV4_SETTINGS_CHANGED | LWIP_NSC_IPV6_SET | LWIP_NSC_IPV6_ADDR_STATE_CHANGED)) {
+		LT_DM(MDNS, "Restarting mDNS");
+		start();
+	}
+}
+#endif
+
 bool mDNS::begin(const char *hostname) {
+	hostName = strdup(hostname);
 	setInstanceName(hostname);
+#ifdef LWIP_NETIF_EXT_STATUS_CALLBACK
+	netif_add_ext_callback(&netif_callback, mdns_netif_ext_status_callback);
+#endif
 	LT_DM(MDNS, "Starting (%s)", hostname);
 #if LWIP_VERSION_MAJOR >= 2 && LWIP_VERSION_MINOR >= 1
 	mdns_resp_register_name_result_cb(mdnsStatusCallback);
 #endif
 	mdns_resp_init();
-	uint8_t enabled = 0;
-
-	struct netif *netif;
-	for (netif = netif_list; netif != NULL; netif = netif->next) {
-		if (!netif_is_up(netif))
-			continue;
-
-		LT_DM(MDNS, "Adding netif %u", netif->num);
-		if ((netif->flags & NETIF_FLAG_IGMP) == 0) {
-			LT_DM(MDNS, "Enabling IGMP");
-			netif->flags |= NETIF_FLAG_IGMP;
-			igmp_start(netif);
-		}
-
-		err_t ret = mdns_resp_add_netif(netif, hostname, 255);
-		if (ret == ERR_OK)
-			enabled++;
-		else
-			LT_DM(MDNS, "Cannot add netif %u; ret=%d, errno=%d", netif->num, ret, errno);
-	}
-	return enabled > 0;
+	return start();
 }
 
 void mDNS::end() {
@@ -103,8 +165,10 @@ bool mDNS::addServiceImpl(const char *name, const char *service, uint8_t proto, 
 		return false;
 
 	// add the service to TXT record arrays
+	services_name.push_back(strdup(name));
 	services.push_back(strdup(service));
 	protos.push_back(proto);
+	ports.push_back(port);
 	records.emplace_back();
 
 	return true;
