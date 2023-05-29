f1e41f7cc15c52d189e82e362c3b388bf2db9c31
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2023-05-29 22:25:23 +0200

    [realtek-ambz2] Add GDB init command

--- cores/realtek-ambz2/base/lt_api.c
+++ cores/realtek-ambz2/base/lt_api.c
@@ -7,7 +7,7 @@ extern uint8_t lt_uart_port;
 
 void lt_init_family() {
 	// make the SDK less verbose by default
-	ConfigDebugErr	= 0;
+	ConfigDebugErr	= _DBG_MISC_ | _DBG_FAULT_ | _DBG_BOOT_;
 	ConfigDebugWarn = 0;
 	ConfigDebugInfo = 0;
 	lt_uart_port	= LT_UART_DEFAULT_PORT;
