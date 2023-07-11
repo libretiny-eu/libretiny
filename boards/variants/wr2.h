6169f68119616bad6c4222ad6a6db903135b88b0
Sonic-Amiga <48695031+Sonic-Amiga@users.noreply.github.com>
2023-07-11 12:46:36 +0300

    [realtek-ambz] Improve flashing guide, add test pads for WR2 board (#147)
    * platform/realtek-ambz: Improve flashing guide
    
    Known USB adapter compatibility issues. Emphasize, so that the paragraph is
    easier to notice.
    
    Signed-off-by: Pavel Fedin <pavel_fedin@mail.ru>
    
    * base/wr2: Add UART2 test pads to the WR2 diagram
    
    The module has UART2, necessary for flashing, only on test pads. Document this.
    
    Signed-off-by: Pavel Fedin <pavel_fedin@mail.ru>
    
    * Move test pads to separate files, update variants
    
    * Update wiring guide
    
    ---------
    
    Signed-off-by: Pavel Fedin <pavel_fedin@mail.ru>
    Co-authored-by: Kuba Szczodrzyński <kuba@szczodrzynski.pl>

--- boards/variants/wr2.h
+++ boards/variants/wr2.h
@@ -6,14 +6,16 @@
 
 // Pins
 // ----
-#define PINS_COUNT         8  // Total GPIO count
-#define NUM_DIGITAL_PINS   7  // Digital inputs/outputs
+#define PINS_COUNT         10 // Total GPIO count
+#define NUM_DIGITAL_PINS   9  // Digital inputs/outputs
 #define NUM_ANALOG_INPUTS  1  // ADC inputs
-#define NUM_ANALOG_OUTPUTS 6  // PWM & DAC outputs
+#define NUM_ANALOG_OUTPUTS 8  // PWM & DAC outputs
 #define PINS_GPIO_MAX      41 // Last usable GPIO number
 
 // Wire Interfaces
 // ---------------
+#define PIN_WIRE0_SCL 29u // PA_29
+#define PIN_WIRE0_SDA 30u // PA_30
 #define PIN_WIRE1_SCL 18u // PA_18
 #define PIN_WIRE1_SDA 23u // PA_23
 
@@ -21,6 +23,8 @@
 // ------------
 #define PIN_SERIAL0_RX 18u // PA_18
 #define PIN_SERIAL0_TX 23u // PA_23
+#define PIN_SERIAL2_RX 29u // PA_29
+#define PIN_SERIAL2_TX 30u // PA_30
 
 // Pin function macros
 // -------------------
@@ -34,24 +38,32 @@
 #define PIN_PA15  15u // PA_15
 #define PIN_PA18  18u // PA_18
 #define PIN_PA23  23u // PA_23
+#define PIN_PA29  29u // PA_29
+#define PIN_PA30  30u // PA_30
 #define PIN_PWM0  14u // PA_14
 #define PIN_PWM1  15u // PA_15
 #define PIN_PWM2  0u  // PA_0
 #define PIN_PWM3  12u // PA_12
-#define PIN_PWM4  5u  // PA_5
+#define PIN_PWM4  29u // PA_29
 #define PIN_RX0   18u // PA_18
+#define PIN_RX2   29u // PA_29
 #define PIN_SCK0  18u // PA_18
 #define PIN_SCK1  18u // PA_18
+#define PIN_SCL0  29u // PA_29
 #define PIN_SCL1  18u // PA_18
+#define PIN_SDA0  30u // PA_30
 #define PIN_SDA1  23u // PA_23
 #define PIN_TX0   23u // PA_23
+#define PIN_TX2   30u // PA_30
 
 // Port availability
 // -----------------
 #define HAS_SERIAL0             1
+#define HAS_SERIAL2             1
+#define HAS_WIRE0               1
 #define HAS_WIRE1               1
-#define SERIAL_INTERFACES_COUNT 1
-#define WIRE_INTERFACES_COUNT   1
+#define SERIAL_INTERFACES_COUNT 2
+#define WIRE_INTERFACES_COUNT   2
 
 // Arduino pin names
 // -----------------
@@ -62,6 +74,8 @@
 #define PIN_D5 23u // PA_23
 #define PIN_D6 14u // PA_14
 #define PIN_D7 15u // PA_15
+#define PIN_D8 30u // PA_30
+#define PIN_D9 29u // PA_29
 #define PIN_A1 41u // AD_2
 
 // Static pin names
@@ -74,3 +88,5 @@ static const unsigned char D4 = PIN_D4;
 static const unsigned char D5 = PIN_D5;
 static const unsigned char D6 = PIN_D6;
 static const unsigned char D7 = PIN_D7;
+static const unsigned char D8 = PIN_D8;
+static const unsigned char D9 = PIN_D9;
