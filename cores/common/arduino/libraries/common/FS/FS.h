8f6b6a964ee92fb15745538d94cbeee6dde406dd
kolos <kolos@users.noreply.github.com>
2023-12-13 13:39:26 +0100

    clang format FS folder

--- cores/common/arduino/libraries/common/FS/FS.h
+++ cores/common/arduino/libraries/common/FS/FS.h
@@ -21,9 +21,9 @@
 #ifndef FS_H
 #define FS_H
 
-#include <memory>
-#include <Arduino.h>
 #include <../include/time.h> // See issue #6714
+#include <Arduino.h>
+#include <memory>
 
 class SDClass;
 
@@ -41,250 +41,262 @@ class DirImpl;
 typedef std::shared_ptr<DirImpl> DirImplPtr;
 
 template <typename Tfs>
-bool mount(Tfs& fs, const char* mountPoint);
+bool mount(Tfs &fs, const char *mountPoint);
 
-enum SeekMode {
-    SeekSet = 0,
-    SeekCur = 1,
-    SeekEnd = 2
-};
+enum SeekMode { SeekSet = 0, SeekCur = 1, SeekEnd = 2 };
 
-class File : public Stream
-{
-public:
-    File(FileImplPtr p = FileImplPtr(), FS *baseFS = nullptr) : _p(p), _fakeDir(nullptr), _baseFS(baseFS) { }
+class File : public Stream {
+  public:
+	File(FileImplPtr p = FileImplPtr(), FS *baseFS = nullptr) : _p(p), _fakeDir(nullptr), _baseFS(baseFS) {}
 
-    // Print methods:
-    size_t write(uint8_t) override;
-    size_t write(const uint8_t *buf, size_t size) override;
-    int availableForWrite() override;
+	// Print methods:
+	size_t write(uint8_t) override;
+	size_t write(const uint8_t *buf, size_t size) override;
+	int availableForWrite() override;
 
-    // Stream methods:
-    int available() override;
-    int read() override;
-    int peek() override;
-    void flush() override;
-    size_t readBytes(char *buffer, size_t length) {
-        return read((uint8_t*)buffer, length);
-    }
-    int read(uint8_t* buf, size_t size);
-    bool seek(uint32_t pos, SeekMode mode);
-    bool seek(uint32_t pos) {
-        return seek(pos, SeekSet);
-    }
-    size_t position() const;
-    size_t size() const;
-    virtual ssize_t streamRemaining() { return (ssize_t)size() - (ssize_t)position(); }
-    void close();
-    operator bool() const;
-    const char* name() const;
-    const char* fullName() const; // Includes path
-    bool truncate(uint32_t size);
+	// Stream methods:
+	int available() override;
+	int read() override;
+	int peek() override;
+	void flush() override;
 
-    bool isFile() const;
-    bool isDirectory() const;
+	size_t readBytes(char *buffer, size_t length) {
+		return read((uint8_t *)buffer, length);
+	}
 
-    // Arduino "class SD" methods for compatibility
-    //TODO use stream::send / check read(buf,size) result
-    template<typename T> size_t write(T &src){
-      uint8_t obuf[256];
-      size_t doneLen = 0;
-      size_t sentLen;
+	int read(uint8_t *buf, size_t size);
+	bool seek(uint32_t pos, SeekMode mode);
 
-      while (src.available() > (int)sizeof(obuf)){
-        src.read(obuf, sizeof(obuf));
-        sentLen = write(obuf, sizeof(obuf));
-        doneLen = doneLen + sentLen;
-        if(sentLen != sizeof(obuf)){
-          return doneLen;
-        }
-      }
+	bool seek(uint32_t pos) {
+		return seek(pos, SeekSet);
+	}
 
-      size_t leftLen = src.available();
-      src.read(obuf, leftLen);
-      sentLen = write(obuf, leftLen);
-      doneLen = doneLen + sentLen;
-      return doneLen;
-    }
-    using Print::write;
+	size_t position() const;
+	size_t size() const;
 
-    void rewindDirectory();
-    File openNextFile();
+	virtual ssize_t streamRemaining() {
+		return (ssize_t)size() - (ssize_t)position();
+	}
 
-    String readString();
+	void close();
+	operator bool() const;
+	const char *name() const;
+	const char *fullName() const; // Includes path
+	bool truncate(uint32_t size);
 
-    time_t getLastWrite();
-    time_t getCreationTime();
-    void setTimeCallback(time_t (*cb)(void));
+	bool isFile() const;
+	bool isDirectory() const;
 
-    // Stream::send configuration
+	// Arduino "class SD" methods for compatibility
+	// TODO use stream::send / check read(buf,size) result
+	template <typename T>
+	size_t write(T &src) {
+		uint8_t obuf[256];
+		size_t doneLen = 0;
+		size_t sentLen;
 
-    bool inputCanTimeout () {
-        // unavailable data can't become later available
-        return false;
-    }
+		while (src.available() > (int)sizeof(obuf)) {
+			src.read(obuf, sizeof(obuf));
+			sentLen = write(obuf, sizeof(obuf));
+			doneLen = doneLen + sentLen;
+			if (sentLen != sizeof(obuf)) {
+				return doneLen;
+			}
+		}
 
-    bool outputCanTimeout () {
-        // free space for write can't increase later
-        return false;
-    }
+		size_t leftLen = src.available();
+		src.read(obuf, leftLen);
+		sentLen = write(obuf, leftLen);
+		doneLen = doneLen + sentLen;
+		return doneLen;
+	}
 
-protected:
-    FileImplPtr _p;
-    time_t (*_timeCallback)(void) = nullptr;
+	using Print::write;
 
-    // Arduino SD class emulation
-    std::shared_ptr<Dir> _fakeDir;
-    FS                  *_baseFS;
+	void rewindDirectory();
+	File openNextFile();
+
+	String readString();
+
+	time_t getLastWrite();
+	time_t getCreationTime();
+	void setTimeCallback(time_t (*cb)(void));
+
+	// Stream::send configuration
+
+	bool inputCanTimeout() {
+		// unavailable data can't become later available
+		return false;
+	}
+
+	bool outputCanTimeout() {
+		// free space for write can't increase later
+		return false;
+	}
+
+  protected:
+	FileImplPtr _p;
+	time_t (*_timeCallback)(void) = nullptr;
+
+	// Arduino SD class emulation
+	std::shared_ptr<Dir> _fakeDir;
+	FS *_baseFS;
 };
 
 class Dir {
-public:
-    Dir(DirImplPtr impl = DirImplPtr(), FS *baseFS = nullptr): _impl(impl), _baseFS(baseFS) { }
+  public:
+	Dir(DirImplPtr impl = DirImplPtr(), FS *baseFS = nullptr) : _impl(impl), _baseFS(baseFS) {}
 
-    File openFile(const char* mode);
+	File openFile(const char *mode);
 
-    String fileName();
-    size_t fileSize();
-    time_t fileTime();
-    time_t fileCreationTime();
-    bool isFile() const;
-    bool isDirectory() const;
+	String fileName();
+	size_t fileSize();
+	time_t fileTime();
+	time_t fileCreationTime();
+	bool isFile() const;
+	bool isDirectory() const;
 
-    bool next();
-    bool rewind();
+	bool next();
+	bool rewind();
 
-    void setTimeCallback(time_t (*cb)(void));
+	void setTimeCallback(time_t (*cb)(void));
 
-protected:
-    DirImplPtr _impl;
-    FS       *_baseFS;
-    time_t (*_timeCallback)(void) = nullptr;
+  protected:
+	DirImplPtr _impl;
+	FS *_baseFS;
+	time_t (*_timeCallback)(void) = nullptr;
 };
 
 // Backwards compatible, <4GB filesystem usage
 struct FSInfo {
-    size_t totalBytes;
-    size_t usedBytes;
-    size_t blockSize;
-    size_t pageSize;
-    size_t maxOpenFiles;
-    size_t maxPathLength;
+	size_t totalBytes;
+	size_t usedBytes;
+	size_t blockSize;
+	size_t pageSize;
+	size_t maxOpenFiles;
+	size_t maxPathLength;
 };
 
 // Support > 4GB filesystems (SD, etc.)
 struct FSInfo64 {
-    uint64_t totalBytes;
-    uint64_t usedBytes;
-    size_t blockSize;
-    size_t pageSize;
-    size_t maxOpenFiles;
-    size_t maxPathLength;
+	uint64_t totalBytes;
+	uint64_t usedBytes;
+	size_t blockSize;
+	size_t pageSize;
+	size_t maxOpenFiles;
+	size_t maxPathLength;
 };
 
+class FSConfig {
+  public:
+	static constexpr uint32_t FSId = 0x00000000;
 
-class FSConfig
-{
-public:
-    static constexpr uint32_t FSId = 0x00000000;
+	FSConfig(uint32_t type = FSId, bool autoFormat = true) : _type(type), _autoFormat(autoFormat) {}
 
-    FSConfig(uint32_t type = FSId, bool autoFormat = true) : _type(type), _autoFormat(autoFormat) { }
+	FSConfig setAutoFormat(bool val = true) {
+		_autoFormat = val;
+		return *this;
+	}
 
-    FSConfig setAutoFormat(bool val = true) {
-        _autoFormat = val;
-        return *this;
-    }
-
-    uint32_t _type;
-    bool     _autoFormat;
+	uint32_t _type;
+	bool _autoFormat;
 };
 
-class SPIFFSConfig : public FSConfig
-{
-public:
-    static constexpr uint32_t FSId = 0x53504946;
-    SPIFFSConfig(bool autoFormat = true) : FSConfig(FSId, autoFormat) { }
+class SPIFFSConfig : public FSConfig {
+  public:
+	static constexpr uint32_t FSId = 0x53504946;
 
-    // Inherit _type and _autoFormat
-    // nothing yet, enableTime TBD when SPIFFS has metadate
+	SPIFFSConfig(bool autoFormat = true) : FSConfig(FSId, autoFormat) {}
+
+	// Inherit _type and _autoFormat
+	// nothing yet, enableTime TBD when SPIFFS has metadate
 };
 
-class FS
-{
-public:
-    FS(FSImplPtr impl) : _impl(impl) { _timeCallback = _defaultTimeCB; }
+class FS {
+  public:
+	FS(FSImplPtr impl) : _impl(impl) {
+		_timeCallback = _defaultTimeCB;
+	}
 
-    bool setConfig(const FSConfig &cfg);
+	bool setConfig(const FSConfig &cfg);
 
-    bool begin();
-    void end();
+	bool begin();
+	void end();
 
-    bool format();
-    bool info(FSInfo& info);
-    bool info64(FSInfo64& info);
+	bool format();
+	bool info(FSInfo &info);
+	bool info64(FSInfo64 &info);
 
-    File open(const char* path, const char* mode = "r");
-//    File open(const char* path, const char* mode);
-    File open(const String& path, const char* mode);
+	File open(const char *path, const char *mode = "r");
+	//    File open(const char* path, const char* mode);
+	File open(const String &path, const char *mode);
 
-    bool exists(const char* path);
-    bool exists(const String& path);
+	bool exists(const char *path);
+	bool exists(const String &path);
 
-    Dir openDir(const char* path);
-    Dir openDir(const String& path);
+	Dir openDir(const char *path);
+	Dir openDir(const String &path);
 
-    bool remove(const char* path);
-    bool remove(const String& path);
+	bool remove(const char *path);
+	bool remove(const String &path);
 
-    bool rename(const char* pathFrom, const char* pathTo);
-    bool rename(const String& pathFrom, const String& pathTo);
+	bool rename(const char *pathFrom, const char *pathTo);
+	bool rename(const String &pathFrom, const String &pathTo);
 
-    bool mkdir(const char* path);
-    bool mkdir(const String& path);
+	bool mkdir(const char *path);
+	bool mkdir(const String &path);
 
-    bool rmdir(const char* path);
-    bool rmdir(const String& path);
+	bool rmdir(const char *path);
+	bool rmdir(const String &path);
 
-    // Low-level FS routines, not needed by most applications
-    bool gc();
-    bool check();
+	// Low-level FS routines, not needed by most applications
+	bool gc();
+	bool check();
 
-    time_t getCreationTime();
+	time_t getCreationTime();
 
-    void setTimeCallback(time_t (*cb)(void));
+	void setTimeCallback(time_t (*cb)(void));
 
-    friend class ::SDClass; // More of a frenemy, but SD needs internal implementation to get private FAT bits
-protected:
-    FSImplPtr _impl;
-    FSImplPtr getImpl() { return _impl; }
-    time_t (*_timeCallback)(void) = nullptr;
-    static time_t _defaultTimeCB(void) { return time(NULL); }
+	friend class ::SDClass; // More of a frenemy, but SD needs internal implementation to get private FAT bits
+
+  protected:
+	FSImplPtr _impl;
+
+	FSImplPtr getImpl() {
+		return _impl;
+	}
+
+	time_t (*_timeCallback)(void) = nullptr;
+
+	static time_t _defaultTimeCB(void) {
+		return time(NULL);
+	}
 };
 
 } // namespace fs
 
-extern "C"
-{
+extern "C" {
 void close_all_fs(void);
 void littlefs_request_end(void);
 void spiffs_request_end(void);
 }
 
 #ifndef FS_NO_GLOBALS
-using fs::FS;
-using fs::File;
 using fs::Dir;
-using fs::SeekMode;
-using fs::SeekSet;
+using fs::File;
+using fs::FS;
+using fs::FSConfig;
+using fs::FSInfo;
 using fs::SeekCur;
 using fs::SeekEnd;
-using fs::FSInfo;
-using fs::FSConfig;
+using fs::SeekMode;
+using fs::SeekSet;
 using fs::SPIFFSConfig;
-#endif //FS_NO_GLOBALS
+#endif // FS_NO_GLOBALS
 
 #if !defined(NO_GLOBAL_INSTANCES) && !defined(NO_GLOBAL_SPIFFS)
-extern fs::FS SPIFFS __attribute__((deprecated("SPIFFS has been deprecated. Please consider moving to LittleFS or other filesystems.")));
+extern fs::FS SPIFFS
+	__attribute__((deprecated("SPIFFS has been deprecated. Please consider moving to LittleFS or other filesystems.")));
 #endif
 
-#endif //FS_H
+#endif // FS_H
