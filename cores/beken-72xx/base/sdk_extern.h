e1c5761df992a1634df24308fc82d1e4d30d912f
devgs <devgs@ukr.net>
2024-09-05 15:45:48 +0300

    [beken-72xx] Allow setting deep sleep wake-up pins as floating (#275)
    According to guide `gpio_stay_*_map` are specifically there to prevent
    pulling pins to a specific level and let them float during deep sleep.
    
    https://docs.bekencorp.com/sdk_3.0.x/bk7238/build/en/latest/developer-guide/power_save/sleep_test.html
    
    This allows any external low-power circuitry to change a pin level, as
    required, to trigger the wake up
    
    This requires changes in BDK and was implemented via fixup. Also, exposed
    `gpio_stay_lo_map` parameter via `lt_deep_sleep_keep_floating_gpio` to
    manage this configuration for pins in range P0-P31.

--- cores/beken-72xx/base/sdk_extern.h
+++ cores/beken-72xx/base/sdk_extern.h
@@ -18,7 +18,7 @@ uint32_t wdt_ctrl(uint32_t cmd, void *param);
 void bk_send_byte(uint8_t uport, uint8_t data);
 void uart_hw_set_change(uint8_t uport, bk_uart_config_t *uart_config);
 int uart_rx_callback_set(int uport, uart_callback callback, void *param);
-void sctrl_enter_rtos_deep_sleep(PS_DEEP_CTRL_PARAM *deep_param);
+void sctrl_enter_rtos_deep_sleep_fix(PS_DEEP_CTRL_PARAM *deep_param);
 void ps_delay(volatile UINT16 times);
 
 #ifdef __cplusplus
