7fe87f6b55c1ec615ed13022169ba95c791e2f7b
lamauny <pub.elc@free.fr>
2025-02-09 09:56:35 +0100

    [ln882x] removed personal script file

--- docs/platform/lightning-ln882x/debug_openocd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#sudo openocd -f /usr/share/openocd/scripts/interface/stlink.cfg -f ../../../cores/lightning-ln882x/misc/ln882x.cfg -c 'reset_config srst_only' $*
-sudo openocd -f /usr/share/openocd/scripts/interface/jlink.cfg -f ../../../cores/lightning-ln882x/misc/ln882x.cfg -c 'reset_config srst_only' $*
