74518c3f55f7a2ee3ad6c5dcfc2392638ebc30c9
lamauny <pub.elc@free.fr>
2025-02-09 09:54:35 +0100

    [ln882x] removed Beken auto-download command

--- cores/lightning-ln882h/base/lt_family.h
+++ cores/lightning-ln882h/base/lt_family.h
@@ -16,7 +16,3 @@
 #error "No serial port is available"
 #endif
 #endif
-
-// Auto-download-reboot detection pattern
-// Link check command (CMD_LinkCheck=0)
-#define LT_UART_ADR_PATTERN 0x01, 0xE0, 0xFC, 0x01, 0x00
