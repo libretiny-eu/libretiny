06c288c25721fd9fbc459248313452a2276796e9
lamauny <pub.elc@free.fr>
2025-02-28 17:15:38 +0100

    [ln882x] cleanup code

--- cores/lightning-ln882h/base/api/lt_ota.c
+++ cores/lightning-ln882h/base/api/lt_ota.c
@@ -1,8 +1,8 @@
 /* Copyright (c) Etienne Le Cousin 2024-12-21. */
 
 #include <libretiny.h>
-#include <sdk_private.h>
 #include <ota_image.h>
+#include <sdk_private.h>
 
 lt_ota_type_t lt_ota_get_type() {
 	return OTA_TYPE_SINGLE;
@@ -11,15 +11,15 @@ lt_ota_type_t lt_ota_get_type() {
 bool lt_ota_is_valid(uint8_t index) {
 	image_hdr_t ota_header;
 
-	if ( OTA_ERR_NONE != image_header_fast_read(FLASH_OTA_OFFSET, &ota_header) ) {
+	if (OTA_ERR_NONE != image_header_fast_read(FLASH_OTA_OFFSET, &ota_header)) {
 		return false;
 	}
 
-	if ( OTA_ERR_NONE != image_header_verify(&ota_header) ) {
+	if (OTA_ERR_NONE != image_header_verify(&ota_header)) {
 		return false;
 	}
 
-	if ( OTA_ERR_NONE != image_body_verify(FLASH_OTA_OFFSET, &ota_header)) {
+	if (OTA_ERR_NONE != image_body_verify(FLASH_OTA_OFFSET, &ota_header)) {
 		return false;
 	}
 	return true;
