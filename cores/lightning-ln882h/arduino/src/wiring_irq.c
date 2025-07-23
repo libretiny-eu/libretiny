a1ed208309453c148627779c5ec9b2b5a5e73a47
Luca Olivetti <luca@ventoso.org>
2025-07-23 21:33:28 +0200

    only call NVIC_DisableIRQ if no other gpio is using an interrupt

--- cores/lightning-ln882h/arduino/src/wiring_irq.c
+++ cores/lightning-ln882h/arduino/src/wiring_irq.c
@@ -89,8 +89,17 @@ void attachInterruptParam(pin_size_t interruptNumber, voidFuncPtrParam callback,
 void detachInterrupt(pin_size_t interruptNumber) {
 	pinCheckGetData(interruptNumber, PIN_IRQ, );
 
-	uint16_t IRQForPin = interruptNumber < 16 ? GPIOA_IRQn : GPIOB_IRQn;
-	NVIC_DisableIRQ(IRQForPin);
 	hal_gpio_pin_it_en(data->gpio_base, data->gpio->pin, HAL_DISABLE);
 	pinModeRemove(interruptNumber, PIN_IRQ);
+
+	//only call NVIC_DisableIRQ if no gpio in the same bank is using the interrupt
+	for (pin_size_t pinNumber = (interruptNumber < 16 ? 0 : 16); pinNumber < (interruptNumber < 16 ? 16 : 32); pinNumber++) {
+		PinInfo *pin = pinInfo(pinNumber);
+		if (!pin)
+			continue;
+		if (pinEnabled(pin, PIN_IRQ))
+			return;
+	}
+	uint16_t IRQForPin=interruptNumber < 16 ? GPIOA_IRQn : GPIOB_IRQn;
+	NVIC_DisableIRQ(IRQForPin);
 }
