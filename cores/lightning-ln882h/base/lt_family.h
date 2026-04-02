0e9b9b57cd26003e3d5c2cc1c416e6463a97b6cd
Bl00d-B0b <efilistovic@gmail.com>
2026-04-02 17:41:46 +0300

    [lightning-ln882h] Fix default log UART port to UART1 (#373)
    Per Tuya WL2H-U datasheet, UART1 (PB8/PB9) is the designated log output
    port on LN882H modules. UART0 (PA2/PA3) is the user UART and is typically
    connected to GPIOs on CB3S-footprint boards (e.g. indicator LEDs on the
    DS-101JL wall switch). Using UART0 as the default log port was introduced
    in #312 and caused PA2/PA3 to be claimed by AFIO, preventing their use
    as GPIO.
    
    Change lt_family.h to prefer UART1 over UART0 as the default, consistent
    with how beken-72xx defaults to UART2/UART1 and avoids UART0.

--- cores/lightning-ln882h/base/lt_family.h
+++ cores/lightning-ln882h/base/lt_family.h
@@ -5,11 +5,13 @@
 #include <lt_pins.h>
 
 // Choose the main UART output port
+// Per Tuya WL2H-U datasheet, UART1 (PB8/PB9) is the designated log port;
+// UART0 (PA2/PA3) is the user UART and should remain free as GPIO.
 #ifndef LT_UART_DEFAULT_PORT
-#if LT_HW_UART0
-#define LT_UART_DEFAULT_PORT 0
-#elif LT_HW_UART1
+#if LT_HW_UART1
 #define LT_UART_DEFAULT_PORT 1
+#elif LT_HW_UART0
+#define LT_UART_DEFAULT_PORT 0
 #elif LT_HW_UART2
 #define LT_UART_DEFAULT_PORT 2
 #else
